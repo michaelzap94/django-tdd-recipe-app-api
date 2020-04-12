7e5e09d0dcada411d66af8ffd52982b4740ccb3c
Michael Zapata <mikeyfriends@hotmail.com>
2020-04-13 00:21:36 +0100

    Added base user app

--- app/RecipeAPIProject/settings.py
+++ app/RecipeAPIProject/settings.py
@@ -32,12 +32,15 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'core.apps.CoreConfig',
+    'user.apps.UserConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
