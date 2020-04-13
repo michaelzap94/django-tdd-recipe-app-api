f777fead0a1c805b4f3559c96f48e54de924cef6
Michael Zapata <mikeyfriends@hotmail.com>
2020-04-13 01:05:08 +0100

    changed comment

--- app/user/views.py
+++ app/user/views.py
@@ -14,6 +14,6 @@ class CreateTokenView(ObtainAuthToken):
     """Create a new auth token for the user"""
     #override attribute from ObtainAuthToken
     serializer_class = AuthTokenSerializer
-    # (optional) sets the renderer so we can view this endpoint in the browser using Chrome and extension,
+    # (optional) sets the renderer UI so we can view A UI in the browser to create this object,
     #  otherwise we'll need Postman
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
